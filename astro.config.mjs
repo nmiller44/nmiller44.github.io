273926b47319deb2ddbba66dee8f70f19a2da4d6
Neil Miller <neildmiller@gmail.com>
2024-10-29 19:16:58 -0500

    Init Astro, Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nmiller44.github.io',
+    integrations: [tailwind()],
+});
